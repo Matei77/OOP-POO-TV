80c20861371472a0f459a982280ae3dd5e6f813c
Matei77 <matei.ionescu7@gmail.com>
2022-12-16 21:53:59 +0200

    Add changePage action

--- src/engine/PlatformActions.java
+++ src/engine/PlatformActions.java
@@ -1,7 +1,9 @@
 package engine;
 
 import inputHandler.ActionInput;
-import utils.ErrorHandler;
+import user.Movie;
+import utils.OutputHandler;
+import utils.Utils;
 
 import java.util.ArrayList;
 
@@ -17,11 +19,64 @@ public final class PlatformActions {
     String currentPage = PlatformEngine.getEngine().getCurrentPage();
     String nextPage = currentAction.getPage();
 
+    if (currentPage.equals(LOGIN_PAGE) || currentPage.equals(REGISTER_PAGE)) {
+      OutputHandler.updateOutput(ERROR_STATUS);
+    }
+
     if (currentPage.equals(LOGGED_OUT_HOMEPAGE) &&
         !(nextPage.equals(LOGIN_PAGE) || nextPage.equals(REGISTER_PAGE))) {
-      ErrorHandler.updateOutput(ERROR_STATUS);
+      OutputHandler.updateOutput(ERROR_STATUS);
       return;
     }
+
+    if (nextPage.equals(LOGOUT_PAGE)) {
+      PlatformEngine.getEngine().setCurrentUser(null);
+      PlatformEngine.getEngine().setCurrentPage(LOGGED_OUT_HOMEPAGE);
+      return;
+    }
+
+    if (currentPage.equals(MOVIES_PAGE) && nextPage.equals(SEE_DETAILS_PAGE)) {
+      String selectedMovieName = currentAction.getMovie();
+      Movie SelectedMovie = Utils.findMovie(selectedMovieName);
+
+      if (SelectedMovie == null) {
+        OutputHandler.updateOutput(ERROR_STATUS);
+        return;
+      }
+
+      ArrayList<Movie> currentMoviesList = new ArrayList<>();
+      currentMoviesList.add(SelectedMovie);
+      PlatformEngine.getEngine().setCurrentMoviesList(currentMoviesList);
+      PlatformEngine.getEngine().setCurrentPage(nextPage);
+
+      OutputHandler.updateOutput(SUCCESS_STATUS);
+    }
+
+    if (currentPage.equals(LOGGED_IN_HOMEPAGE) && !(nextPage.equals(MOVIES_PAGE)
+        || nextPage.equals(UPGRADES_PAGE))) {
+      OutputHandler.updateOutput(ERROR_STATUS);
+      return;
+    }
+
+    if (currentPage.equals(MOVIES_PAGE) && !(nextPage.equals(LOGGED_IN_HOMEPAGE)
+        || nextPage.equals(SEE_DETAILS_PAGE))) {
+      OutputHandler.updateOutput(ERROR_STATUS);
+      return;
+    }
+
+    if (currentPage.equals(SEE_DETAILS_PAGE) && !(nextPage.equals(LOGGED_IN_HOMEPAGE)
+        || nextPage.equals(MOVIES_PAGE) || nextPage.equals(UPGRADES_PAGE))) {
+      OutputHandler.updateOutput(ERROR_STATUS);
+      return;
+    }
+
+    if (currentPage.equals(UPGRADES_PAGE) && !(nextPage.equals(LOGGED_IN_HOMEPAGE)
+        || nextPage.equals(MOVIES_PAGE))) {
+      OutputHandler.updateOutput(ERROR_STATUS);
+      return;
+    }
+
+    PlatformEngine.getEngine().setCurrentPage(nextPage);
   }
 
   public static void executeActions(final ArrayList<ActionInput> actions) {
