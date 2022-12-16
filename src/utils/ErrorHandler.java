6070e86ef7b119cd3edc2bebb43149da2281f5d4
Matei77 <matei.ionescu7@gmail.com>
2022-12-16 20:12:25 +0200

    Add output handler

---
+++ src/utils/ErrorHandler.java
@@ -0,0 +1,130 @@
+package utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import engine.PlatformActions;
+import engine.PlatformEngine;
+import inputHandler.ActionInput;
+import user.Movie;
+import user.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static utils.Constants.ERROR_STATUS;
+
+public final class ErrorHandler {
+
+  private ErrorHandler() {
+  }
+
+  public static void updateOutput(final int status) {
+    ObjectMapper mapper = new ObjectMapper();
+
+    ObjectNode outputNode = mapper.createObjectNode();
+    if (status == ERROR_STATUS) {
+      outputNode.put("error", "Error");
+    } else {
+      outputNode.set("error", null);
+    }
+
+    ArrayList<Movie> currentMoviesList = PlatformEngine.getEngine().getCurrentMoviesList();
+    ArrayNode currentMoviesOutput = mapper.createArrayNode();
+
+    for (Movie movie : currentMoviesList) {
+      ObjectNode movieObjectNode = createMovieOutput(mapper, movie);
+      currentMoviesOutput.add(movieObjectNode);
+    }
+    outputNode.set("currentMoviesList", currentMoviesOutput);
+
+
+    User currentUser = PlatformEngine.getEngine().getCurrentUser();
+    ObjectNode currentUsersOutput = createUserOutput(mapper, currentUser);
+
+    outputNode.set("currentUser", currentUsersOutput);
+
+    ArrayNode output = PlatformEngine.getEngine().getOutput();
+    output.addAll(List.of(outputNode));
+  }
+
+  private static ObjectNode createMovieOutput(final ObjectMapper mapper, final Movie movie) {
+    ObjectNode movieObjectNode = mapper.createObjectNode();
+
+    movieObjectNode.put("name", movie.getName());
+    movieObjectNode.put("year", movie.getYear());
+    movieObjectNode.put("duration", movie.getDuration());
+    movieObjectNode.put("numLikes", movie.getNumLikes());
+    movieObjectNode.put("rating", movie.getRating());
+    movieObjectNode.put("numRatings", movie.getNumRatings());
+
+    ArrayNode genresOutput = mapper.createArrayNode();
+    ArrayList<String> genres = movie.getGenres();
+    for (String genre : genres) {
+      genresOutput.add(genre);
+    }
+    movieObjectNode.set("genres", genresOutput);
+
+    ArrayNode actorsOutput = mapper.createArrayNode();
+    ArrayList<String> actors = movie.getActors();
+    for (String actor : actors) {
+      actorsOutput.add(actor);
+    }
+    movieObjectNode.set("actors", actorsOutput);
+
+    ArrayNode countriesBannedOutput = mapper.createArrayNode();
+    ArrayList<String> countriesBanned = movie.getCountriesBanned();
+    for (String country : countriesBanned) {
+      countriesBannedOutput.add(country);
+    }
+    movieObjectNode.set("countriesBanned", countriesBannedOutput);
+
+    return movieObjectNode;
+  }
+
+  private static ObjectNode createUserOutput(final ObjectMapper mapper, final User user) {
+    ObjectNode userObjectNode = mapper.createObjectNode();
+
+    ObjectNode credentialsObjectNode = mapper.createObjectNode();
+    credentialsObjectNode.put("name", user.getName());
+    credentialsObjectNode.put("password", user.getPassword());
+    credentialsObjectNode.put("accountType", user.getAccountType());
+    credentialsObjectNode.put("country", user.getCountry());
+    credentialsObjectNode.put("balance", user.getBalance());
+
+    userObjectNode.set("credentials", credentialsObjectNode);
+
+    userObjectNode.put("tokensCount", user.getTokensCount());
+    userObjectNode.put("numFreePremiumMovies", user.getNumFreePremiumMovies());
+
+    ArrayNode purchasedMoviesOutput = mapper.createArrayNode();
+    ArrayList<Movie> purchasedMovies = user.getPurchasedMovies();
+    for (Movie movie : purchasedMovies) {
+      purchasedMoviesOutput.add(movie.getName());
+    }
+    userObjectNode.set("purchasedMovies", purchasedMoviesOutput);
+
+    ArrayNode watchedMoviesOutput = mapper.createArrayNode();
+    ArrayList<Movie> watchedMovies = user.getWatchedMovies();
+    for (Movie movie : watchedMovies) {
+      watchedMoviesOutput.add(movie.getName());
+    }
+    userObjectNode.set("watchedMovies", watchedMoviesOutput);
+
+    ArrayNode likedMoviesOutput = mapper.createArrayNode();
+    ArrayList<Movie> likedMovies = user.getLikedMovies();
+    for (Movie movie : likedMovies) {
+      likedMoviesOutput.add(movie.getName());
+    }
+    userObjectNode.set("likedMovies", likedMoviesOutput);
+
+    ArrayNode ratedMoviesOutput = mapper.createArrayNode();
+    ArrayList<Movie> ratedMovies = user.getRatedMovies();
+    for (Movie movie : ratedMovies) {
+      ratedMoviesOutput.add(movie.getName());
+    }
+    userObjectNode.set("ratedMovies", ratedMoviesOutput);
+
+    return userObjectNode;
+  }
+}
