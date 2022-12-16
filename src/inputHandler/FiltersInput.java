d0e824ecb121b11bbdda311710a92625e7bee25f
Matei77 <matei.ionescu7@gmail.com>
2022-12-16 17:16:58 +0200

    Add Input Handler

---
+++ src/inputHandler/FiltersInput.java
@@ -0,0 +1,22 @@
+package inputHandler;
+
+public final class FiltersInput {
+  private SortInput sort;
+  private ContainsInput contains;
+
+  public SortInput getSort() {
+    return sort;
+  }
+
+  public void setSort(SortInput sort) {
+    this.sort = sort;
+  }
+
+  public ContainsInput getContains() {
+    return contains;
+  }
+
+  public void setContains(ContainsInput contains) {
+    this.contains = contains;
+  }
+}
---
+++ src/inputHandler/UserInput.java
@@ -0,0 +1,13 @@
+package inputHandler;
+
+public final class UserInput {
+  private CredentialsInput credentials;
+
+  public CredentialsInput getCredentials() {
+    return credentials;
+  }
+
+  public void setCredentials(CredentialsInput credentials) {
+    this.credentials = credentials;
+  }
+}
